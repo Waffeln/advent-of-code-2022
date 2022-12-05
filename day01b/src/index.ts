9e9510bd6aadc19ffd77f6fcf36955e611007d4b
waffeln <nils@kaster.space>
2022-12-05 16:32:48 +0100

    add day01b

---
+++ day01b/src/index.ts
@@ -0,0 +1,24 @@
+import {readFile} from "fs";
+
+readFile("./src/assets/input.txt", "utf-8", (err: any, data: string)=>{
+	if(err) {
+		return console.log(err)
+	}
+	const caloriesPerElfArray = data.split("\n")
+	const numberOfTopCalories = 3
+	const mostCaloriesArray = Array(numberOfTopCalories).fill(0);
+	let currentElfCalories = 0
+
+	caloriesPerElfArray.map((el: string)=>{
+		if(!el) {
+			currentElfCalories = 0
+			return;
+		}
+		const minTopCalories = Math.min(...mostCaloriesArray)
+		currentElfCalories += Number(el)
+		if(currentElfCalories > minTopCalories) mostCaloriesArray[mostCaloriesArray.indexOf(minTopCalories)] = currentElfCalories
+	})
+	let sumTopCalories = 0
+	mostCaloriesArray.map((el)=>sumTopCalories += el)
+	console.log(sumTopCalories)
+})
