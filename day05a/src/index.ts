6d5c630fd1fee379dccbb9438d73ed3642a8d447
waffeln <nils@kaster.space>
2022-12-09 22:42:01 +0100

    add day05a

---
+++ day05a/src/index.ts
@@ -0,0 +1,45 @@
+import { readFile } from 'fs'
+
+readFile('./src/assets/input.txt', 'utf-8', (err: any, data: string) => {
+  if (err !== null) {
+    console.error(err)
+  }
+  const cleaningSectionArray: string[] = data.split('\n')
+  const crateStackArray: string[][] = []
+  let rearrangedCrateStackUpperCrate: string = ''
+  let isStartingStack = true
+
+  cleaningSectionArray.forEach((el: string, idx): void => {
+    if (el === '') return undefined
+
+    if (isStartingStack) {
+      if (el[1] === '1') {
+        isStartingStack = false
+        return undefined
+      }
+      for (let x = 0; (x * 4 + 1) < el.length - 1; x++) {
+        // TODO: find less hardcody to see if there is a crate in Stack x |[N]     [F] [M]     [D] [V] [R] [N]|
+        //                                                                | 1   2   3   4   5   6   7   8   9 |
+        if (idx === 0) crateStackArray.push([])
+        if (el[(x * 4) + 1] !== ' ') crateStackArray[x].unshift(el[x * 4 + 1])
+      }
+    } else {
+      let simplifiedString = el.replace('move ', '')
+      simplifiedString = simplifiedString.replace(' from ', '-')
+      simplifiedString = simplifiedString.replace(' to ', '-')
+      const commandRow = simplifiedString.split('-')
+      const fromStack = Number(commandRow[1]) - 1
+      const toStack = Number(commandRow[2]) - 1
+      const fromStackLength = crateStackArray[fromStack].length - 1
+      for (let x = fromStackLength; x > (fromStackLength - Number(commandRow[0])); x--) {
+        crateStackArray[toStack].push(crateStackArray[fromStack][x])
+        crateStackArray[fromStack].pop()
+        if (x < 0) return
+      }
+    }
+  })
+  crateStackArray.forEach((el) => {
+    rearrangedCrateStackUpperCrate += el[el.length - 1]
+  })
+  console.log(rearrangedCrateStackUpperCrate)
+})
