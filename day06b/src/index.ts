92db8e9e11021d86c343d75ec69fd266a07eae2d
waffeln <nils@kaster.space>
2022-12-10 14:50:49 +0100

    add day06b

---
+++ day06b/src/index.ts
@@ -0,0 +1,25 @@
+import { readFile } from 'fs'
+
+readFile('./src/assets/input.txt', 'utf-8', (err: any, data: string) => {
+  if (err !== null) {
+    console.error(err)
+  }
+  const bufferInputArray: string[] = data.replace('\n', '').split('')
+  const characterArray: string[] = []
+
+  for (let x = 0; x < bufferInputArray.length; x++) {
+    if (characterArray.length === 14) {
+      let isNotStartMarker = false
+      characterArray.forEach((el, idx) => {
+        const tempCharacterArray = [...characterArray]
+        tempCharacterArray.splice(idx, 1)
+        if (tempCharacterArray.includes(el)) isNotStartMarker = true
+      })
+      if (!isNotStartMarker) {
+        console.log(x, characterArray)
+        break
+      } else characterArray.pop()
+    }
+    characterArray.unshift(bufferInputArray[x])
+  }
+})
